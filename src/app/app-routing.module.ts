fb3f8b689235db3fcd98649d0ffabd958a58a1fc
InformatycznyInformatyk <igorswat2002@o2.pl>
2024-05-22 18:46:07 +0200

    Init Angular project & routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
-const routes: Routes = [];
+// Routing path examples
+const routes: Routes = [
+  // {path: '', component: StartViewComponent},
+  // {path: 'data', component: DataViewComponent},
+  // {path: '**', component: PageNotFoundComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes), CommonModule],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
