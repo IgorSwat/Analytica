50a84c0967ad6fce7b3ad9b903c88bf8aabce000
zmija <norbertzmija@student.agh.edu.pl>
2024-05-31 20:45:32 +0200

    Added new endpoints

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -13,19 +13,37 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 
 app.secret_key = "TEST"
 
-df = {}
+df = pd.DataFrame()
 
 @app.route('/upload', methods=["POST"])
-def uploadFile():
+def upload_file():
+    global df
+    if 'file' not in request.files:
+        return {"reason" : "No file xselected"}, 400
+    file = request.files['file']
+    if file.filename == '':
+        return {"reason" : "File not named"}, 400
+    try:
+        content = file.stream.read(1024)
+        file.stream.seek(0)
+        dialect = csv.Sniffer().sniff(content.decode('utf-8'))
+        df = pd.read_csv(file, delimiter=dialect.delimiter)
 
-    f = request.files.get('file')
- 
-    dialect = csv.Sniffer().sniff(f.stream.readline().decode('utf-8'))
-    
-    df["data"] = pd.read_csv(filepath_or_buffer=f, delimiter=dialect.delimiter)
-    df["delimiter"] = dialect.delimiter
+        return {"message": "File processed successfully"}
 
-    return jsonify({"message": dialect.delimiter}) 
+    except Exception as e:
+        return {"reason": str(e)}, 500
+
+@app.route("/data/visualize", methods=['GET'])
+def visualize_data():
+    global df
+    rowAmt = df.shape[0]
+    cols = df.columns.values.tolist()
+    head = df.head(5).values.tolist()
+    tail = df.tail(5).values.tolist()
+    head = [[str(x) for x in row] for row in head]
+    tail = [[str(x) for x in row] for row in tail]
+    return jsonify(length=rowAmt,columns=cols,head=head,tail=tail),200
     
 
 if __name__ == "__main__":
